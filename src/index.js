5c6779e4961100ea5e67f8803db479532bbbdf4e
AnnaFutryk <anna.futryk@gmail.com>
2025-03-19 21:00:02 +0200

    create project

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./styles.css";
+
+console.log("Webpack and Tailwind are working!");
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[contenthash].css" }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true,
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    hot: true,
+    open: true,
+  },
+};
